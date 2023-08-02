2a7657437eb187baba0209859a9499cd713b9718
pengxianhong <290677893@qq.com>
2023-08-03 02:59:12 +0800

    perf(*): 调整UDP界面以及逻辑

--- CSharpDemo/ViewModels/UdpServerViewModel.cs
+++ CSharpDemo/ViewModels/UdpServerViewModel.cs
@@ -24,49 +24,72 @@ namespace CSharpDemo.ViewModels
             }
         }
 
+        private ObservableCollection<string> _connectedLinks = new ObservableCollection<string>();
+
+        public ObservableCollection<string> ConnectedLinks
+        {
+            get => _connectedLinks;
+            set
+            {
+                _connectedLinks = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public DelegateCommand<UserControl> ViewOnUnloadedCommand { set; get; }
         public DelegateCommand<ListBox> ItemSelectedCommand { set; get; }
+        public DelegateCommand StartServerCommand { set; get; }
 
         public UdpServerViewModel()
         {
-            var server = new AppServer();
-            if (!server.Setup(new ServerConfig
-                {
-                    Ip = "Any",
-                    Port = 7777,
-                    Mode = SocketMode.Udp
-                })) //配置服务器
+            ViewOnUnloadedCommand = new DelegateCommand<UserControl>(delegate(UserControl control)
             {
-                Debug.WriteLine("UdpServerViewModel => 配置服务器失败！");
-                return;
-            }
-
-            //获取本地IP
-            var hostAddress = SystemHelper.GetHostAddress();
-            Debug.WriteLine($"UdpServerViewModel => {hostAddress}");
-
-            if (!server.Start())
-            {
-                Debug.WriteLine("UdpServerViewModel => 启动失败！");
-                return;
-            }
-
-            Messages.Add($"服务端{hostAddress}:7777已启动");
-
-            server.NewSessionConnected += NewSessionConnected;
-            server.NewRequestReceived += NewRequestReceived;
+                //页面移除的时候触发
+            });
 
             ItemSelectedCommand = new DelegateCommand<ListBox>(delegate(ListBox box)
             {
                 Debug.WriteLine($"UdpServerViewModel => {box.SelectedItem}");
             });
+
+            StartServerCommand = new DelegateCommand(delegate
+            {
+                var server = new AppServer();
+                if (!server.Setup(new ServerConfig
+                    {
+                        Ip = "Any",
+                        Port = 7777,
+                        Mode = SocketMode.Udp
+                    })) //配置服务器
+                {
+                    Debug.WriteLine("UdpServerViewModel => 配置服务器失败！");
+                    return;
+                }
+
+                //获取本地IP
+                var hostAddress = SystemHelper.GetHostAddress();
+                Debug.WriteLine($"UdpServerViewModel => {hostAddress}");
+
+                if (!server.Start())
+                {
+                    Debug.WriteLine("UdpServerViewModel => 启动失败！");
+                    return;
+                }
+
+                Messages.Add($"服务端{hostAddress}:7777已启动");
+
+                server.NewSessionConnected += NewSessionConnected;
+                server.NewRequestReceived += NewRequestReceived;
+            });
         }
 
-        private static void NewSessionConnected(AppSession session)
+        private void NewSessionConnected(AppSession session)
         {
             Debug.WriteLine($"UdpServerViewModel => {session.SessionID} Connected");
+            ConnectedLinks.Add($"{session.RemoteEndPoint.Address}:{session.RemoteEndPoint.Port}");
         }
 
-        private static void NewRequestReceived(AppSession session, StringRequestInfo requestInfo)
+        private void NewRequestReceived(AppSession session, StringRequestInfo requestInfo)
         {
             var body = requestInfo.Body;
             Debug.WriteLine($"UdpServerViewModel => {body}");
