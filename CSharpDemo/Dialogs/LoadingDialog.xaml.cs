2c9c3d3d6f61345c5138f65056b1c54c5d432730
pengxianhong <290677893@qq.com>
2023-10-30 00:24:50 +0800

    style(*): 优化加载动画

--- CSharpDemo/Dialogs/LoadingDialog.xaml.cs
+++ CSharpDemo/Dialogs/LoadingDialog.xaml.cs
@@ -1,37 +1,14 @@
-﻿using System;
-using System.Windows;
-using System.Windows.Threading;
+﻿using System.Windows;
 
 namespace CSharpDemo.Dialogs
 {
     public partial class LoadingDialog : Window
     {
-        //运行时间Timer
-        private readonly DispatcherTimer _runningTimer = new DispatcherTimer
-        {
-            Interval = TimeSpan.FromSeconds(1)
-        };
-
-        //计算时间
-        private int _runningSeconds;
-
         public LoadingDialog(string message)
         {
             InitializeComponent();
-            _runningTimer.Tick += delegate
-            {
-                _runningSeconds++;
-                Application.Current.Dispatcher.Invoke(delegate
-                {
-                    LoadingView.LoadingText = message + $"{_runningSeconds}s";
-                });
-            };
-            _runningTimer.Start();
-        }
 
-        private void LoadingDialog_OnClosed(object sender, EventArgs e)
-        {
-            _runningTimer.Stop();
+            LoadingView.LoadingText = message;
         }
     }
 }
