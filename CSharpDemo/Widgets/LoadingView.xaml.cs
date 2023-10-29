1c6dfa19c09ef41538c3228539c7e51afcf87ac6
pengxianhong <290677893@qq.com>
2023-10-29 19:52:36 +0800

    feat(*): 扇形进度条初版

---
+++ CSharpDemo/Widgets/LoadingView.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace CSharpDemo.Widgets
+{
+    public partial class LoadingView : UserControl
+    {
+        public static readonly DependencyProperty LoadingTextProperty = DependencyProperty.Register(
+            nameof(LoadingText), typeof(string), typeof(LoadingView),
+            new PropertyMetadata("Loading...")
+        );
+
+        //声明属性        
+        public string LoadingText
+        {
+            get => (string)GetValue(LoadingTextProperty);
+            set => SetValue(LoadingTextProperty, value);
+        }
+
+        public LoadingView()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+    }
+}
