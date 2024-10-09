adf0603a8ea331fcbb264a370209a3a6a8529def
pengxianhong <290677893@qq.com>
2024-10-09 21:40:25 +0800

    实现主题切换（整体颜色细节未完）

--- CSharpDemo/Views/MainWindow.xaml.cs
+++ CSharpDemo/Views/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System;
+using System.Linq;
+using System.Windows;
 
 namespace CSharpDemo.Views
 {
@@ -10,6 +12,32 @@ namespace CSharpDemo.Views
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void ToggleButton_OnChecked(object sender, RoutedEventArgs e)
+        {
+            var mergedDictionaries = Application.Current.Resources.MergedDictionaries;
+
+            // 移除当前主题  
+            var dictionary = mergedDictionaries.Last();
+            mergedDictionaries.Remove(dictionary);
+
+            // 添加新主题  
+            var newTheme = new ResourceDictionary { Source = new Uri("Themes/DarkTheme.xaml", UriKind.Relative) };
+            mergedDictionaries.Add(newTheme);
+        }
+
+        private void ToggleButton_OnUnchecked(object sender, RoutedEventArgs e)
+        {
+            var mergedDictionaries = Application.Current.Resources.MergedDictionaries;
+
+            // 移除当前主题  
+            var dictionary = mergedDictionaries.Last();
+            mergedDictionaries.Remove(dictionary);
+
+            // 添加新主题  
+            var newTheme = new ResourceDictionary { Source = new Uri("Themes/LightTheme.xaml", UriKind.Relative) };
+            mergedDictionaries.Add(newTheme);
         }
     }
 }
